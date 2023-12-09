41d867b07be47a3d1770720fa57760bd60fe8368
Tristan Kremser <tristan.kremser@etu.emse.fr>
2023-12-09 15:03:08 +0100

    maj

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
@@ -45,7 +44,6 @@ dependencies {
     implementation ("com.google.code.gson:gson:2.8.6")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("androidx.appcompat:appcompat:1.3.1")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.3")
     implementation ("com.google.android.gms:play-services-maps:18.2.0")
     implementation ("com.squareup.okhttp3:okhttp:4.9.1")
@@ -58,11 +56,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
 
-
-
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
 }
